69d571f3440b4558816bfbd6fcc585b246cb5ed2
artgrtmnk <78636533+artgrtmnk@users.noreply.github.com>
2022-07-25 19:03:59 +0400

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,7 @@ require 'page-object/page_factory'
 require 'webdrivers'
 require 'solid_assert'
 
-ENV['GOREST_TOKEN']="Bearer 594add4a6e80e6bfeb2b345424060aad3bba0d538628eeff491d774957ee834a"
+ENV['GOREST_TOKEN']="Bearer PASTE_YOUR_TOKEN_OVER_HERE"
 
 begin
   $driver = Selenium::WebDriver.for :chrome
